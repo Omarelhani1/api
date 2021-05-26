88becf24f29157d7fa1be2a1832e678236ba0371
Omar.elhani <omar.elhani1@gmail.com>
2021-05-26 12:21:38 +0200

    Pagina buscar continente añadida

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def inicio():
 	return render_template("inicio.html")
 
+@app.route('/buscar_pais',methods=["GET","POST"])
+def buscar_pais():
+	return render_template("buscar_pais.html")
+
+@app.route('/buscar_continente',methods=["GET","POST"])
+def buscar_continente():
+	return render_template("buscar_continente.html")
+
 port=os.environ["PORT"]
 app.run('0.0.0.0', int(port), debug=False)
