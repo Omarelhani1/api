0c9a49a35d7bcc959bbf099e2de25c51a450c541
Omar.elhani <omar.elhani1@gmail.com>
2021-05-30 19:21:23 +0200

    Casi terminado y Procfile añadido

--- app.py
+++ app.py
@@ -16,22 +16,51 @@ def buscar_pais():
     if request.method == "GET":
         return render_template("buscar_pais.html", nombre_pais=nombre_pais)
     else:
+        r1=requests.get(url_base1+nombre_pais)
+        if r1.status_code==200:
+            parametros={"country":nombre_pais}
+            r=requests.get(url_base+'/cases',params=parametros)
+            doc=r.json()
+            confirmados=doc.get("All").get("confirmed")
+            recuperados=doc.get("All").get("recovered")
+            muertos=doc.get("All").get("deaths")
+            poblacion=doc.get("All").get("population")
+            return render_template("buscar_pais.html",nombre_pais=nombre_pais,confirmados=confirmados,recuperados=recuperados,muertos=muertos,poblacion=poblacion)
+        else:
+            return abort(404)
+
+@app.route('/buscar_ciudad',methods=["GET","POST"])
+def buscar_ciudad():
+    nombre_pais=request.form.get("nombre")
+    nombre_ciudad=request.form.get("nombre1")
+    if request.method == "GET":
+        return render_template("buscar_ciudad.html",nombre_ciudad=nombre_ciudad, nombre_pais=nombre_pais)
+    else:
         parametros={"country":nombre_pais}
         r=requests.get(url_base+'/cases',params=parametros)
         doc=r.json()
-        confirmados=doc.get("All").get("confirmed")
-        recuperados=doc.get("All").get("recovered")
-        muertos=doc.get("All").get("deaths")
-        poblacion=doc.get("All").get("population")
-        return render_template("buscar_pais.html",nombre_pais=nombre_pais,confirmados=confirmados,recuperados=recuperados,muertos=muertos,poblacion=poblacion)
-
-@app.route('/buscar_continente',methods=["GET","POST"])
-def buscar_continente():
-	return render_template("buscar_continente.html")
+        confirmados=doc.get(nombre_ciudad).get("confirmed")
+        recuperados=doc.get(nombre_ciudad).get("recovered")
+        muertos=doc.get(nombre_ciudad).get("deaths")
+        return render_template("buscar_ciudad.html",nombre_ciudad=nombre_ciudad,nombre_pais=nombre_pais,confirmados=confirmados,recuperados=recuperados,muertos=muertos)
 
 @app.route('/buscar_iniciales',methods=["GET","POST"])
 def buscar_iniciales():
-	return render_template("buscar_iniciales.html")
-
+    inicial=request.form.get("nombre")
+    if request.method == "GET":
+        return render_template("buscar_iniciales.html", inicial=inicial)
+    else:
+        r1=requests.get(url_base2+inicial)
+        if r1.status_code==200:
+            parametros={"ab":inicial}
+            r=requests.get(url_base+'/cases',params=parametros)
+            doc=r.json()
+            confirmados=doc.get("All").get("confirmed")
+            recuperados=doc.get("All").get("recovered")
+            muertos=doc.get("All").get("deaths")
+            poblacion=doc.get("All").get("population")
+            return render_template("buscar_iniciales.html",inicial=inicial,confirmados=confirmados,recuperados=recuperados,muertos=muertos,poblacion=poblacion)
+        else:
+            return abort(404)
 port=os.environ["PORT"]
 app.run('0.0.0.0', int(port), debug=False)
